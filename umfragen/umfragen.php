9f779a945955dfe4a8c1d4441ed971bec52b52fc
akuigat <akuigat@yahoo.de>
2018-11-21 20:25:12 +0100

    design schon angefangen

--- umfragen/umfragen.php
+++ umfragen/umfragen.php
@@ -3,12 +3,32 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Umfragen</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="umfragen.css" />
+    <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Monoton|Oxygen" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="main.js"></script>
 </head>
 <body>
+<header class="app-header">
+
+     <div class="logo"><a class="logo-link" href="#home">free-poll</a> </div>
+     <div class="recherche"> hello center </div>
+     <div class="menus"> 
+         <a href="notification"><i class="fas fa-bell"></i></a>
+
+         <img src="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/11250201_774899555953883_1414390562393057489_n.jpg?_nc_cat=111&_nc_ht=scontent-frt3-2.xx&oh=9a57884127b80dc7b7f1298d7e817d5c&oe=5C7A1A20" alt="Avatar" class="avatar">
+    
+    </div>
+
+</header>
+<section class = "contenu_principal">
+</section>
+
+
     
 </body>
 </html>
