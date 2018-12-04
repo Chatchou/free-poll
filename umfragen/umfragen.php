42eb843d096e59dc167a83fe553c5278f72971c5
akuigat <akuigat@yahoo.de>
2018-12-05 00:40:33 +0100

    index.htlm

--- umfragen/umfragen.php
+++ umfragen/umfragen.php
@@ -16,13 +16,15 @@
 <header class="app-header">
 
      <div class="logo"><a class="logo-link" href="#home">free-poll</a> </div>
-     <div class="recherche"> hello center </div>
+     <div class="rechercheer"> hello center </div>
      <div class="menus"> 
          <a href="notification"><i class="fas fa-bell"></i></a>
 
          <img src="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/11250201_774899555953883_1414390562393057489_n.jpg?_nc_cat=111&_nc_ht=scontent-frt3-2.xx&oh=9a57884127b80dc7b7f1298d7e817d5c&oe=5C7A1A20" alt="Avatar" class="avatar">
     
     </div>
+
+     
 
 </header>
 <section class = "contenu_principal">
