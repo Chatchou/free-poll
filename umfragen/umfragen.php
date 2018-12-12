d5fdcd8bd5f4cdc47c3138a4fcd4c0780ef5db9d
akuigat <akuigat@yahoo.de>
2018-12-12 17:20:52 +0100

    Header fertig

--- umfragen/umfragen.php
+++ umfragen/umfragen.php
@@ -10,10 +10,35 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Monoton|Oxygen" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
     <script src="main.js"></script>
 </head>
 <body>
+  <header class="container-fluid header">
+       
+      
+          <a href="#" class="logo">
+             <img src="img/logo.png" alt="logo" class="logo_img">
+              <h1>free-pool</h1>
+          </a>
+         
+      
+          <div class="searchbar">
+             <form>
+                <input type="text" placeholder="search" name="search">
+             </form>
+         </div>
 
+          <nav class="menu">
+              <a href=""><i class="fas fa-plus"></i></a>
+              <a href=""><i class="fas fa-bell"></i></a>
+              <a href=""><img src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/11250201_774899555953883_1414390562393057489_n.jpg?_nc_cat=111&_nc_ht=scontent-frt3-1.xx&oh=f48956a7d0387c1fa2d3db251df0e217&oe=5CA1A720" alt="avatar" class="avatar"></a>
+              <a href=""><i class="fas fa-ellipsis-h"></i></a>
+          </nav>
+          
+     
+</header>
+  <section class="container-fluid main"></section>
 
 </body>
 </html>
