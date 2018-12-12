7011aa6abe6dee5d623d03d0e9eba767a393f58c
akuigat <akuigat@yahoo.de>
2018-12-12 20:47:52 +0100

    Umfrage Seite

--- umfragen/umfragen.php
+++ umfragen/umfragen.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="umfragen.css" />
     <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
     <link href="https://fonts.googleapis.com/css?family=Monoton|Oxygen" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
@@ -38,7 +38,72 @@
           
      
 </header>
-  <section class="container-fluid main"></section>
+  <section class="main">
+      <div class="grid-container">
+          
+               <div class="card">
+                   <div class="card-body">
+                       <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png" alt="umfrage_card" alt="card-image" class="card-image">
+                   </div>
+                   <div class="card-footer">
+                       <a href="" class="title"><h3>Umfrage title</h3> open on 12/12/2018</a>
+                       <a href="" class="pop"><i class="fas fa-ellipsis-h"></i></a>
+                   </div>
+                </div>   
+                
+                 <div class="card">
+                   <div class="card-body">
+                       <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png" alt="umfrage_card" alt="card-image" class="card-image">
+                   </div>
+                   <div class="card-footer">
+                       <a href="" class="title"><h3>Umfrage title</h3> open on 12/12/2018</a>
+                       <a href="" class="pop"><i class="fas fa-ellipsis-h"></i></a>
+                   </div>
+                </div>   
+
+                 <div class="card">
+                   <div class="card-body">
+                       <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png" alt="umfrage_card" alt="card-image" class="card-image">
+                   </div>
+                   <div class="card-footer">
+                       <a href="" class="title"><h3>Umfrage title</h3> open on 12/12/2018</a>
+                       <a href="" class="pop"><i class="fas fa-ellipsis-h"></i></a>
+                   </div>
+                </div>   
+
+                 <div class="card">
+                   <div class="card-body">
+                       <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png" alt="umfrage_card" alt="card-image" class="card-image">
+                   </div>
+                   <div class="card-footer">
+                       <a href="" class="title"><h3>Umfrage title</h3> open on 12/12/2018</a>
+                       <a href="" class="pop"><i class="fas fa-ellipsis-h"></i></a>
+                   </div>
+                </div>   
+
+                 <div class="card">
+                   <div class="card-body">
+                       <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png" alt="umfrage_card" alt="card-image" class="card-image">
+                   </div>
+                   <div class="card-footer">
+                       <a href="" class="title"><h3>Umfrage title</h3> open on 12/12/2018</a>
+                       <a href="" class="pop"><i class="fas fa-ellipsis-h"></i></a>
+                   </div>
+                </div>   
+
+                 <div class="card">
+                   <div class="card-body">
+                       <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png" alt="umfrage_card" alt="card-image" class="card-image">
+                   </div>
+                   <div class="card-footer">
+                       <a href="" class="title"><h3>Umfrage title</h3> open on 12/12/2018</a>
+                       <a href="" class="pop"><i class="fas fa-ellipsis-h"></i></a>
+                   </div>
+                </div>   
+                
+         
+      </div>
+  </section>
 
 </body>
 </html>
